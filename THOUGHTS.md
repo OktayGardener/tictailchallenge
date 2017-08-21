7f12f3dd381d449df1ce44393e8a63adf983b5ac
Oktay Bahceci <oktayb@kth.se>
2017-08-21 15:18:55 +0200

    add pandas strategy, add documentation & cosmetics

--- THOUGHTS.md
+++ THOUGHTS.md
@@ -5,3 +5,13 @@ Plan:
 4 Filters: GPS->Limited shops, sort by popularity items
 5 Tags functionality
 6 Optimize
+
+Pandas plan:
+0 Set index to be ID for all
+1 Find Closest shops (run the distance thing, take all given distance)
+2 Filter and get all items
+3 Get all items for those shops, sort by popularity
+If tags:
+4 Filter optionally by tag
+Else return
+Jsonify results if needed
--- server/api.py
+++ server/api.py
@@ -17,10 +17,12 @@ def parse_csv_files():
     load_data()
 
 def data_path(filename):
+    """ Return correct path for a given filename """
     data_path = current_app.config['DATA_PATH']
     return u"%s/%s" % (data_path, filename)
 
 def load_data():
+    """ Load all available datasets and set to current app instance """
     current_app.shops = pd.read_csv(data_path('shops.csv'), index_col=0)
     current_app.products = pd.read_csv(data_path('products.csv'), index_col=0)
     current_app.tags = pd.read_csv(data_path('tags.csv'), index_col=0)
@@ -28,6 +30,15 @@ def load_data():
     current_app.taggings.drop(['id'], axis=1, inplace=True)
 
 def item_request_query(count, radius, lat, lng, tags=None):
+    """ Serve a new request query given params
+    Parameters:
+    -----------
+    count: int
+    radius: int
+    lat: float
+    lng: float
+    tags: list
+    """
     current_app.shops['dist'] = haversine_np(lng, lat,
     current_app.shops['lng'], current_app.shops['lat'])
 
@@ -35,18 +46,31 @@ def item_request_query(count, radius, lat, lng, tags=None):
     closest_shops = shops.sort_values(['dist'], ascending=False).index.tolist()
 
     if tags:
-        closest_shops = filter_items_tags(closest_shops, tags)
+        closest_shops = shops_with_tag(closest_shops, tags)
 
     items = most_popular_items(closest_shops, count)
 
     return items['title'].tolist()
 
 def most_popular_items(shops, n):
+    """ Return n most popular items from a list of shops
+    Parameters:
+    -----------
+    shops: list
+    n: int
+    """
     items = current_app.products[current_app.products['shop_id'].isin(shops)]
     popular = items.sort_values(['popularity'], ascending=False)
     return popular.head(n)
 
-def filter_items_tags(shops, tags):
+def shops_with_tag(shops, tags):
+    """ Return filtered shops that has given tags.
+    If there are no such shops, return original list.
+    Parameters:
+    -----------
+    shops: list
+    tags: list
+    """
     if len(current_app.tags['tag'].isin(tags)) == 0:
         return shops
     else:
@@ -58,6 +82,15 @@ def filter_items_tags(shops, tags):
         return shops_with_tags
 
 def haversine_np(lon1, lat1, lon2, lat2):
+    """ Calculates the haversine between one point and an array of points,
+    return the distance in meter.
+    Parameters:
+    -----------
+    lon1: float
+    lat1: float
+    lon2: list
+    lat2: list
+    """
     lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
 
     dlon = lon2 - lon1
@@ -70,14 +103,22 @@ def haversine_np(lon1, lat1, lon2, lat2):
     return m
 
 def validate_request(args):
+    """ Validate the parameters of a GET request.
+    Use init values if anything is missing.
+    Parameters:
+    -----------
+    args: dict
+    """
     valid_args = defaultdict()
-    params = [('count', int), ('lat', float), ('lng', float), ('radius', int)]
+    params = [('count', int, 10), ('lat', float, 59.3325800), \
+    ('lng', float, 18.0649000), ('radius', int, 100)]
 
-    for param, dtype in params:
+    for param, dtype, init in params:
         try:
             valid_args[param] = args.get(param, type=dtype)
         except ValueError:
-            pass
+            valid_args[param] = init
+
     tags = args.get('tags', None)
     if tags:
         valid_args['tags'] = map(lambda tag: tag.strip(), tags.split(','))
@@ -86,7 +127,5 @@ def validate_request(args):
 @api.route('/search', methods=['GET'])
 def search():
     params = validate_request(request.args)
-    print(params)
     items = item_request_query(**params)
-    print(items)
     return jsonify({'products': items})
